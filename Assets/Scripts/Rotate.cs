b0b45402c1601a0f07192bacbbcd1ea21db3bd10
Daniel Ilett <danielilett@gmail.com>
2020-03-20 13:42:08 +0000

    Added Ethan model example.

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private Vector3 startPos;
+
+    private void Awake()
+    {
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = startPos + new Vector3(0.0f, Mathf.Sin(Time.time) * 0.025f, 0.0f);
+
+        transform.rotation = Quaternion.Euler(0.0f, (Time.time * 30.0f) % 360.0f, 0.0f);
+    }
+}
