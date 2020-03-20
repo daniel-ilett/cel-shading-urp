86d674fbba1bc7431f97741bd01b9cdfe15d1a1c
Daniel Ilett <danielilett@gmail.com>
2020-03-20 20:04:14 +0000

    Added temporary graph for tutorial content.

--- Assets/Shaders/GetLighting.hlsl
+++ Assets/Shaders/GetLighting.hlsl
@@ -17,15 +17,3 @@ void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out h
 	Attenuation = mainLight.distanceAttenuation * mainLight.shadowAttenuation;
 #endif
 }
-
-void DirectSpecular_half(half3 Specular, half Smoothness, half3 Direction, half3 Color, half3 WorldNormal, half3 WorldView, out half3 Out)
-{
-#if SHADERGRAPH_PREVIEW
-	Out = 0;
-#else
-	Smoothness = exp2(10 * Smoothness + 1);
-	WorldNormal = normalize(WorldNormal);
-	WorldView = SafeNormalize(WorldView);
-	Out = LightingSpecular(Color, Direction, WorldNormal, WorldView, half4(Specular, 0), Smoothness);
-#endif
-}
